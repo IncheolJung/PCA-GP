1d97e6bf0aa340a9951f80e74f42a18adcd6e543
incheol <jungin1107@gmail.com>
2025-10-21 18:33:50 -0500

    mpi gather broken_sim_dirs to root

--- datareader.py
+++ datareader.py
@@ -515,6 +515,7 @@ class OnFlySolver:
     def _add_freq(self, new_freq):
         if not isinstance(new_freq, Iterable): new_freq = [new_freq]
         if self.usempi:
+            print(f"[Rank: {self.rank}] calling _add_freq")
             freqs = self.comm.gather(new_freq, root=0)
             if self.rank == 0:
                 new_freq = [item for sublist in freqs for item in sublist]
@@ -525,6 +526,7 @@ class OnFlySolver:
     
     def _add_farfield(self, new_farfield):
         if self.usempi:
+            print(f"[Rank: {self.rank}] calling _add_farfield")
             farfields_local_keys, farfields_local_vals = zip(*new_farfield.items())
             farfields_keys = self.comm.gather(farfields_local_keys, root=0)
             farfields_vals = self.comm.gather(farfields_local_vals, root=0)

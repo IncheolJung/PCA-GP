1f6fcbff3981269637ca1d5cb16f0b184ddfc564
incheol <jungin1107@gmail.com>
2025-10-22 10:11:55 -0500

    mpi gather broken_sim_dirs to root

--- main.py
+++ main.py
@@ -318,6 +318,8 @@ def main():
     # -----------------------
     # OUTPUT DRIECTORY SETUP
     # -----------------------
+    if usempi:
+        print(f"  >> Simulation path: [Rank {rank}] {workingpath}")
     if (not usempi) or (usempi and rank==0):
         dir_out = Path("out")
         simulation_number = len([d for d in dir_out.glob("*") if d.is_dir()]) + 1
@@ -325,10 +327,7 @@ def main():
         dir_out.mkdir()
         stdout = Tee(dir_out/"GP_results.log", "w")     # Log file setup
         print(f"\n ======  Simulation {simulation_number} Initialized  ====== \n")
-        if usempi:
-            print(f"  >> Simulation path: [Rank {rank}] {workingpath}")
-        else:
-            print(f"  >> Simulation path: {workingpath}")
+        print(f"  >> Simulation path: {workingpath}")
         print(f"  >> Adaptive basis: {adaptive_basis}")
         print(f"  >> Acquisition function: {acquisition_function_candidate[acquisition_function]}")
         print(f"  >> X normalization strategy: {Xnormalizer_type_candidate[Xnormalizer_type]}")

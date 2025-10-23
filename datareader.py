fe0bd0ed5c37f753e174b4c5b6fca8db6c27c519
incheol <jungin1107@gmail.com>
2025-10-23 11:18:14 -0500

    working: shared filesystem support

--- datareader.py
+++ datareader.py
@@ -383,6 +383,7 @@ class OnFlySolver:
         print('[Rank 0] Unpacking delivered simulations')
         from subprocess import Popen
         Popen(['mv', f'{self.workingpath}/tmp/*', f'{self.workingpath}/']).wait()
+        Popen(['rm', '-r', f'{self.workingpath}/tmp/']).wait()
         return 0
     
     def _transfer_dir_to_root(self, dirname: Path):

b4d7f95bd818ba5f3bf76f12c9bb77c2513141cb
incheol <jungin1107@gmail.com>
2025-10-23 17:09:54 -0500

    working: shared filesystem support

--- datareader.py
+++ datareader.py
@@ -499,7 +499,7 @@ class OnFlySolver:
                     raise RuntimeError(
                         f"simulation error exit {exit_code} at rank {self.rank}"
                     )
-            self._clean_simulations(delay=0.5*self.rank)
+            self._clean_simulations()
             self.comm.Barrier()
             if self.rank == 0:
                 self._unpack_delivery()

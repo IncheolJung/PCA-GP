01b31a1e7193f73c4c335fcd8a07af2908838d25
Incheol Jung <jungin1107@gmail.com>
2025-10-29 09:49:46 -0500

    bak

--- run_node.sh
+++ run_node.sh
@@ -3,4 +3,4 @@ unset DISPLAY
 export OMP_NUM_THREADS=$(( $(nproc) / 1 ))
 source /home/incheol/.miniconda3/etc/profile.d/conda.sh
 conda activate gpytorch
-python main.py -F 500 1500 1 -s 1 -S 1 -n 8 --add 4 -i 80 -p ./data/VWT-data/sphere -v
+python main.py -F 500 1500 1 -s 1 -S 1 -n 2 --add 2 -i 80 -t 5 -p ./data/VWT-data/sphere -v

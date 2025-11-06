61e57b4eeb3658da681a288abcadb6b988df80f2
Incheol Jung <jungin1107@gmail.com>
2025-11-06 16:25:20 -0600

    modified slides

--- run_node.sh
+++ run_node.sh
@@ -4,4 +4,5 @@ export OMP_NUM_THREADS=$(( $(nproc) / 2 ))
 # source $(conda info --base)/etc/profile.d/conda.sh
 source /home/incheol/.miniconda3/etc/profile.d/conda.sh
 conda activate gpytorch
-python main.py -F 200 400 1 -s 1 -S 1 -n 4 --add 1 -i 50 -t 5 -T 5e-3 -p ./data/VWT-data/circylinder-200MHz -v
+# python main.py -F 230 360 1 -s 1 -S 1 -n 4 --add 1 -i 100 -t 5 -T 5e-3 -p ./data/VWT-data/circylinder-200MHz -v
+python main.py -F 10000 15000 10 -s 1 -S 0 -n 4 --add 1 -i 100 -t 5 -T 5e-3 -p ./data/VWT-data/prime -v

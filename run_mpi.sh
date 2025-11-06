1e771cdd694c36b5432a947ab8e3ae6fc9e26904
Incheol Jung <jungin1107@gmail.com>
2025-11-06 11:45:34 -0600

    minor fix: write_config

--- run_mpi.sh
+++ run_mpi.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 unset DISPLAY
-$HOME/Software/openmpi-4.1.4/bin/mpiexec --hostfile hosts.txt --mca oob_tcp_if_include tailscale0 --mca btl_tcp_if_include tailscale0 --bind-to none --map-by node -x SSH_AUTH_SOCK --prefix /home/incheol/Software/openmpi-4.1.4/ -n 2 $1
+$HOME/Software/openmpi-4.1.4/bin/mpiexec --hostfile hosts.txt --mca oob_tcp_if_include tailscale0 --mca btl_tcp_if_include tailscale0 --bind-to none --map-by node -x SSH_AUTH_SOCK --prefix /home/incheol/Software/openmpi-4.1.4/ -n 4 $1
